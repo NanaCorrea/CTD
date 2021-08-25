fe617cfdbc7c474487c80d4864a9e8fdd2e1c36a
Nana Correa <nana.filosofia@hotmail.com>
2021-08-25 14:29:59 -0300

    Tratamento de erro: Comida Inexistente

--- PI/Semana3/quarta/microondas.js
+++ PI/Semana3/quarta/microondas.js
@@ -11,13 +11,13 @@ function microondas(comida, tempo) {
         console.log("A comida queimou")
     } else if (tempo < comida) {
         console.log("Tempo insuficiente")
-    } else if ((comida != pipoca) && (comida != macarrao) && (comida != carne) && (comida != feijao) && (comida != brigadeiro)) {
-        console.log("Comida inexistente")
-    } else if (tempo > (comida * 3)) {
-        console.log("kabumm")
     } else {
-        console.log("Ocorreu um erro")
+        console.log("kabumm")
     }
 }
 
-microondas(pipoca, 200)
+try {
+    microondas(pipoca, 28)
+} catch (error) {
+    console.log("Comida inexistente")
+}
