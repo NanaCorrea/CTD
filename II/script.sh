f26ec4772992e8e8daf1c330663a85f88c847b32
Nana Correa <nana.filosofia@hotmail.com>
2021-10-26 09:16:30 -0300

    Script sh

---
+++ II/script.sh
@@ -0,0 +1,3 @@
+mkdir -p DH/integracao_aula_1e2/historia_da_contabilidade DH/integracao_aula_1e2/servidores DH/integracao_aula_1e2/interfaces_de_usuario DH/integracao_aula_1e2/infraestruturas DH/integracao_aula_1e2/o_que_faremos
+touch DH/integracao_aula_1e2/historia_da_contabilidade/resumo.txt DH/integracao_aula_1e2/servidores/resumo.txt DH/integracao_aula_1e2/interfaces_de_usuario/resumo.txt DH/integracao_aula_1e2/infraestruturas/resumo.txt DH/integracao_aula_1e2/o_que_faremos/resumo.txt
+ls -R
